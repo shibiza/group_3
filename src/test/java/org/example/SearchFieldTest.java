c8bc34c2494163ec5ce1ccf9b2d20ceeff708f67
Sabina Woźniak <sabinawozniak90@gmail.com>
2022-08-22 11:19:10 +0200

    Task 2 - after amendments

--- src/test/java/org/example/SearchFieldTest.java
+++ src/test/java/org/example/SearchFieldTest.java
@@ -8,83 +8,76 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
 import java.time.Duration;
 
 public class SearchFieldTest {
+    private WebDriver webDriver;
+
+    @BeforeTest
+    public void before() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.get("https://www.amazon.com/");
+        webDriver.manage().window().maximize();
+    }
+
+    @AfterTest
+    public void after() {
+        webDriver.close();
+        webDriver.quit();
+    }
 
     @Test
     public void checkIncorrectText() {
-        WebDriverManager.chromedriver().setup();
+        String searchIncorrectText = "csihbwuvcougw6789okjndso8ycgwubc8herbvgip";
 
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://www.amazon.com/");
-        webDriver.manage().window().maximize();
-
-        String searchIncorrectText = "fdghyhkjtuthhdtkj,kfbvmhn3456789okmnbvcser6789pl,mn cxswert6789ocvb4543u8739698302y5nm,ukl";
-
-        WebElement searchField = webDriver.findElement(By.id("twotabsearchtextbox"));
+        WebElement searchField = new WebDriverWait(webDriver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("twotabsearchtextbox")));
+        searchField.clear(); // After adding @BeforeTest and @afterTest to the code, you need to clear search field
         searchField.sendKeys(searchIncorrectText);
 
         WebElement searchButton = webDriver.findElement(By.id("nav-search-submit-button"));
         searchButton.click();
 
-        WebElement noResultsComment = new WebDriverWait(webDriver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[1]/div/div/div")));
+        WebElement noResultsComment = new WebDriverWait(webDriver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class=\"s-no-outline\"]")));
         Assert.assertEquals("No results for " + searchIncorrectText + ".\nTry checking your spelling or use more general terms", noResultsComment.getText());
-
-        WebElement noResultsComment2 = new WebDriverWait(webDriver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[1]/div/div/div/div[1]/span[1]")));
-        Assert.assertEquals("No results for", noResultsComment2.getText());
-
-        webDriver.close();
-        webDriver.quit();
     }
 
     @Test
     public void checkSearchItem() {
-        WebDriverManager.chromedriver().setup();
-
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://www.amazon.com/");
-        webDriver.manage().window().maximize();
-
         String searchItem = "laptop";
 
-        WebElement searchField = webDriver.findElement(By.id("twotabsearchtextbox"));
+        WebElement searchField = new WebDriverWait(webDriver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("twotabsearchtextbox")));
+        searchField.clear(); // After adding @BeforeTest and @afterTest to the code, you need to clear search field
         searchField.sendKeys(searchItem);
 
         WebElement searchButton = webDriver.findElement(By.id("nav-search-submit-button"));
         searchButton.click();
 
-        WebElement searchingItemIsPresent = new WebDriverWait(webDriver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"search\"]/span/div/h1/div/div[1]/div/div/span[3]")));
+        WebElement searchingItemIsPresent = new WebDriverWait(webDriver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class=\"a-section a-spacing-small a-spacing-top-small\"]/span[3]")));
         Assert.assertEquals("\"" + searchItem + "\"", searchingItemIsPresent.getText());
-
-        webDriver.close();
-        webDriver.quit();
     }
 
     @Test
     public void checkFoundElementsContainsSearchedWord() {
-        WebDriverManager.chromedriver().setup();
-
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://www.amazon.com/");
-        webDriver.manage().window().maximize();
-
         String searchItem = "laptop";
 
-        WebElement searchField = webDriver.findElement(By.id("twotabsearchtextbox"));
+        WebElement searchField = new WebDriverWait(webDriver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("twotabsearchtextbox")));
+        searchField.clear(); // After adding @BeforeTest and @afterTest to the code, you need to clear search field
         searchField.sendKeys(searchItem);
 
         WebElement searchButton = webDriver.findElement(By.id("nav-search-submit-button"));
         searchButton.click();
 
-        boolean searchingWordIsPresent = webDriver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]")).getText().contains(searchItem);
-            Assert.assertTrue(searchingWordIsPresent);
-
-        webDriver.close();
-        webDriver.quit();
+        boolean searchingWordIsPresent = webDriver.findElement(By.xpath("//div[@class=\"s-main-slot s-result-list s-search-results sg-row\"]")).getText().contains(searchItem);
+        Assert.assertTrue(searchingWordIsPresent);
     }
 }
